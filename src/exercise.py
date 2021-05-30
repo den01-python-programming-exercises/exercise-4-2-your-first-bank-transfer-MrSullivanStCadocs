4d9b1478aa7a2d5c7640f55d7a7af0487ddb3ae9
MrSullivan <>
2021-05-30 12:39:07 +0000

    lots

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,17 @@
 def main():
     #write your code below this line
+    Matthews_account = Account("Matthew's account", 1000)
+    my_account = Account("My account", 0)
+
+    Matthews_account.withdraw(100)
+    my_account.deposit(100)
+    print(Matthews_account)
+    print(my_account)
 
 if __name__ == '__main__':
-    main()
+   from account import Account
+   main()
+else:
+   from src.account import Account
+
+
